a72f0ab50b4b24ff2c7511f2b394c4cc3beeb7d2
RoyalFlushever <royalflushdiamond918@gmail.com>
2017-11-23 22:55:37 -0500

    refactoring show pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :videos
-  get 'pages/about'
+	resources :videos
+	get 'pages/about'
 
-  get 'pages/contact'
+	get 'pages/contact'
 
+	root 'videos#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
